f40ef7ceeb4d4842d61d767d2a901b3241d7d58c
Marcelo Henrique Chiaretto <mhchiaretto@gmail.com>
2022-07-10 14:03:25 -0300

    Leitura do conteúdo do arquivo csv

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -14,7 +14,22 @@ class TransactionsController extends Controller
     public function store(Request $request)
     {
         $csvFile = $request->file('csvfile');
+        $file = fopen($csvFile, "r");
+        $transactions = [];
+        $transaction = [];
 
-        dd($csvFile->getClientOriginalName(), ($csvFile->getSize() / 1024 / 1024 ));
+        while (($data = fgetcsv($file))) {
+            $transaction['source_bank'      ] = $data[0];
+            $transaction['source_agency'    ] = $data[1];
+            $transaction['source_account'   ] = $data[2];
+            $transaction['destiny_bank'     ] = $data[3];
+            $transaction['destiny_agency'   ] = $data[4];
+            $transaction['destiny_account'  ] = $data[5];
+            $transaction['value'            ] = $data[6];
+            $transaction['date_time'        ] = $data[7];
+
+            $transactions[] = $transaction;
+        }
+        dd($transactions);
     }
 }
