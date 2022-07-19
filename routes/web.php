f41384345887deb46323a0ccc575dcc657ee0675
Marcelo Henrique Chiaretto <mhchiaretto@gmail.com>
2022-07-18 22:19:04 -0300

    Correção da rota /

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function() {
-    return to_route('login.login');
+    return to_route('login');
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
