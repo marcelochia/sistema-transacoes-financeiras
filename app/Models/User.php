08ca941ef99e0b975ff34800d06ab450936b08d4
Marcelo Henrique Chiaretto <mhchiaretto@gmail.com>
2022-12-04 12:35:03 -0300

    Factory to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use HasFactory;
     use SoftDeletes;
 
     protected $table = 'usuarios';
